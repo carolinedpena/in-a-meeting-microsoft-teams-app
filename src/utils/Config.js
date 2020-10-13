0e0d5bd2006e569c42d85501798c91c2d3d92b91
carolinedpena <47426769+carolinedpena@users.noreply.github.com>
2020-10-13 13:54:31 -0400

    Delete config for security

--- src/utils/Config.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-    appId: '9076cd0e-290b-433e-9b96-49c248317c67',
-    redirectUri: 'http://localhost:3000',
-    scopes: [
-      'user.read',
-      'mailboxsettings.read',
-      'calendars.readwrite'
-    ]
-  };
